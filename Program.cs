6a6bbe553a1072e863ba314a9f8bce314e5deb87
Heisenberg <Persey_98@list.ru>
2020-04-08 19:33:36 +0500

    Создал класс емплое и конструктор

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 
 namespace Homework7_2emplo
 {
+    class Employee
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int experience { get; set; }
+        public string position { get; set; }
+
+        public Employee(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
