d8b619cb5deba2bb752241b10052e2537084d27d
Heisenberg <Persey_98@list.ru>
2020-04-08 19:44:47 +0500

    Создание массивов и вызов класса

--- Program.cs
+++ Program.cs
@@ -20,6 +20,12 @@ namespace Homework7_2emplo
     {
         static void Main(string[] args)
         {
+            string[] positionlist = { "Кассир", "Официант", "Менеджер", "Бухгалтер", "Директор" };
+            double[] salary = { 1400, 1000, 2000, 3000, 5000 };
+
+            Employee em1 = new Employee("Абдурахманов", "Кике");
+            Employee em2 = new Employee("Баротов", "Тошиба");
+            Employee em3 = new Employee("Тестов", "Тестзаде");
 
         }
     }
